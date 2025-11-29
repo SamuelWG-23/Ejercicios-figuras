643388d445e46b62264f091b135af6b12a8e75e5
SamuelWG-23 <samwellwg@alumnos.alborfp.com>
2025-11-29 19:49:42 +0100

    Resolve?

--- Ejercicio 8 Foro.py
+++ Ejercicio 8 Foro.py
@@ -1,4 +1,4 @@
-altura=7
+altura = int(input("Introduce una altura para hacer un rombo sólido de esa altura: "))
 for i in range (1,altura+1,2):
     print(" " * (altura-i) + " *" * i)
 for i in range (altura-2,0,-2):
